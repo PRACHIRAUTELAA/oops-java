32e7f9dced9064809ccd86da327eadf9dfcc2a91
PRACHIRAUTELAA <prachi.rautela.cs28@iilm.edu>
2025-08-30 11:49:57 +0530

    fibonacci series.java

---
+++ fibonacci series.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    static int fibonacciRecursive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+    static void fibonacciIterative(int n) {
+        int first = 0, second = 1;
+        System.out.print("Fibonacci series (Iterative): ");
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        fibonacciIterative(n);
+
+        System.out.print("Fibonacci series (Recursive): ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacciRecursive(i) + " ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
